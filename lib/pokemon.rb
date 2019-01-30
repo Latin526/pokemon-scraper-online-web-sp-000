bd5503584fba6f7d2a1ce8bb2ec30d3a19ce8db8
Yasmin Torres <yasmintorres90@hotmail.com>
2019-01-30 13:33:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,24 +1,33 @@
+require "pry"
+
 class Pokemon
-  attr_accessor :id, :name, :type, :hp, :db 
-  
-  def initialize(id:, name:, type:, hp: nil, db:)
+  attr_reader :id, :name, :type, :hp, :db
+  @@all = []
+
+  def initialize (id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
     @type = type
-    @hp = hp 
+    @hp = hp
     @db = db
+    @@all << self
   end
-  
-  def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon(name,type) VALUES (?, ?)", name, type)
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
-  
-  def self.find(id_num, db)
-    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+
+  def self.find(id, database_connection)
+    pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    name = pokemon[1]
+    type = pokemon[2]
+    hp = pokemon[3]
+
+    pokemon_inst = Pokemon.new(id: id, name: name, type: type, hp: hp, db: database_connection)
   end
-  
-  def alter(new_hp, db)
-    db.execute("UPDATE pokemon SET hp = ?", new_hp, self_id)
+
+  def alter_hp(new_hp, database_connection)
+    database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
   end
+
 end
