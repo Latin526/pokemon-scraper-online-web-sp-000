c94b220918b54825e97244eb46d7698c206af616
Yasmin Torres <yasmintorres90@hotmail.com>
2019-01-30 13:10:32 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,5 +2,10 @@ class Pokemon
   attr_accessor :id, :name, :type, :hp, :db 
   
   def initialize(:id, :name, :type, :hp, :db)
-    @id, @name, @type, @hp, @db = 
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp 
+    @db = db
+  end
 end
